baf471de29547deabb5c170438a72a444a5ed618
JoeNicasio123 <joenicasio123@gmail.com>
2025-01-28 13:36:19 -0500

    finished ui for meal rater

---
+++ app/src/main/java/com/example/mealrater/RateMeal.java
@@ -0,0 +1,15 @@
+package com.example.mealrater;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+public class RateMeal extends AppCompatActivity {
+
+
+}
+
